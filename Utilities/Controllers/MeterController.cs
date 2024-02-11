b0966513855f96445981c5ab81595fe91d46f467
Oleg Sh <oleh.shevchenko.02@gmail.com>
2024-02-11 18:32:50 +0100

    Add all specific controllers

---
+++ Utilities/Controllers/MeterController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using DAL.Dtos;
+using DAL.UnitOfWork;
+using LoggerService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Utilities.Models;
+
+namespace Utilities.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeterController : GenericRestController<Meter, MeterDto>
+    {
+        public MeterController(ILoggerManager logger, IMapper mapper, UnitOfWork unitOfWork)
+            : base(logger, mapper, unitOfWork)
+        {
+
+        }
+    }
+}
